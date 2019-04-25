871d35e4812ddebbffa08f8e16732caabadce646
Jared Lash <jared.c.lash@gmail.com>
2019-04-25 17:25:15 -0500

    Create FileRepository Integration tests, added constructors to File class

--- Dflat.Business/Models/File.cs
+++ Dflat.Business/Models/File.cs
@@ -8,6 +8,21 @@ namespace Dflat.Business.Models
 {
     public class File
     {
+        public File()
+        {
+
+        }
+
+        public File(string filename, string extension, string directory, long size, DateTime lastModifiedTime)
+        {
+            Filename = filename;
+            Extension = extension;
+            Directory = directory;
+            Size = size;
+            LastModifiedTime = lastModifiedTime;
+        }
+
+
         public int FileID { get; set; }
         public string Filename { get; set; }
         public string Extension { get; set; }
