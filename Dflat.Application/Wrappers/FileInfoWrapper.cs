595cf6ab44a757a15ad9c88df34e0f438fae39fb
Jared Lash <jared.c.lash@gmail.com>
2020-04-17 13:18:14 -0500

    Add System.IO wrapper for unit testing
    Created System.IO wrapper class to allow mocking System.IO static classes and FileInfo.

---
+++ Dflat.Application/Wrappers/FileInfoWrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Dflat.Application.Wrappers
+{
+    public class FileInfoWrapper : IFileInfo
+    {
+        private readonly FileInfo fileInfo;
+
+        public FileInfoWrapper(string file)
+        {
+            fileInfo = new FileInfo(file);
+        }
+
+        public string Name => fileInfo.Name;
+
+        public string DirectoryName => fileInfo.DirectoryName;
+
+        public string Extension => fileInfo.Extension;
+
+        public long Length => fileInfo.Length;
+
+        public DateTime LastWriteTime => fileInfo.LastWriteTime;
+    }
+}
