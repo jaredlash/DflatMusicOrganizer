e3bed5c4ceabd514035780f8e6c622f04a71a331
Jared Lash <jared.c.lash@gmail.com>
2020-04-18 17:49:20 -0500

    Begin building out FileSourceFolderScanService
    Adding Job and result models.

---
+++ Dflat.Application/Models/FileSourceFolderScanJob.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dflat.Application.Models
+{
+    public class FileSourceFolderScanJob : Job
+    {
+        public int FileSourceFolderID { get; set; }
+
+
+        public FileSourceFolderScanJob()
+        {
+
+        }
+
+        public FileSourceFolderScanJob(int FileSourceFolderID)
+        {
+            this.FileSourceFolderID = FileSourceFolderID;
+        }
+
+        public override bool SameRequestAs(Job otherJob)
+        {
+            if (!(otherJob is FileSourceFolderScanJob compareJob))
+                return false;
+
+            return compareJob.FileSourceFolderID == FileSourceFolderID;
+        }
+    }
+}
