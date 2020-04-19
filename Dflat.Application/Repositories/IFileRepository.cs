7183477a81dc4ccc9ef7d144ac59f097a61d180e
Jared Lash <jared.c.lash@gmail.com>
2020-04-18 22:30:32 -0500

    Added IFileRepository interface
    Created future interface for mocking

---
+++ Dflat.Application/Repositories/IFileRepository.cs
@@ -0,0 +1,18 @@
+﻿using Dflat.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dflat.Application.Repositories
+{
+    public interface IFileRepository
+    {
+        IEnumerable<File> GetFromPath(string path, bool recurse = true);
+        
+        // Sets FileID on the added file
+        void Add(File modifiedFile);
+        void Update(File modifiedFile);
+
+        void MarkRemoved(int fileID);
+    }
+}
