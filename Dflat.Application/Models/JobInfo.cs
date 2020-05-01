7c53f99614c17ca7983f27bb7cfc34702ff45180
Jared Lash <jared.c.lash@gmail.com>
2020-04-30 19:23:03 -0500

    Make JobInfo implement INotifyPropertyChanged and add utility method to set all properties from existing instance

--- Dflat.Application/Models/JobInfo.cs
+++ Dflat.Application/Models/JobInfo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Dflat.Application.Models
@@ -9,13 +11,88 @@ namespace Dflat.Application.Models
     /// 
     /// Omits the "errors" and "output" fields.
     /// </summary>
-    public class JobInfo
+    public class JobInfo : INotifyPropertyChanged
     {
-        public int JobID { get; set; }
-        public JobType JobType { get; set; }
-        public DateTime CreationTime { get; set; }
-        public string Description { get; set; }
-        public bool IgnoreCache { get; set; }
-        public JobStatus Status { get; set; }
+        private int jobID;
+        private JobType jobType;
+        private DateTime creationTime;
+        private string description;
+        private bool ignoreCache;
+        private JobStatus status;
+
+        public int JobID
+        {
+            get => jobID;
+            set
+            {
+                jobID = value;
+                CallPropertyChanged(nameof(JobID));
+            }
+        }
+        public JobType JobType
+        {
+            get => jobType;
+            set
+            {
+                jobType = value;
+                CallPropertyChanged(nameof(JobType));
+            }
+        }
+        public DateTime CreationTime
+        {
+            get => creationTime;
+            set
+            {
+                creationTime = value;
+                CallPropertyChanged(nameof(CreationTime));
+            }
+        }
+        public string Description
+        {
+            get => description;
+            set
+            {
+                description = value;
+                CallPropertyChanged(nameof(Description));
+            }
+        }
+        public bool IgnoreCache
+        {
+            get => ignoreCache;
+            set
+            {
+                ignoreCache = value;
+                CallPropertyChanged(nameof(IgnoreCache));
+            }
+        }
+        public JobStatus Status
+        {
+            get => status;
+            set
+            {
+                status = value;
+                CallPropertyChanged(nameof(Status));
+            }
+        }
+
+
+        public void SetFromExisting(JobInfo jobInfo)
+        {
+            JobID = jobInfo.JobID;
+            JobType = jobInfo.JobType;
+            CreationTime = jobInfo.CreationTime;
+            Description = jobInfo.Description;
+            IgnoreCache = jobInfo.IgnoreCache;
+            Status = jobInfo.Status;
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void CallPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
