e01cfa381f00ff90f0da88a24b7808a2eaf85071
Jared Lash <jared.c.lash@gmail.com>
2020-05-03 09:02:25 -0500

    Add JobChangeType to JobChangeEventArgs

--- Dflat.Application/Services/JobServices/JobChangeEventArgs.cs
+++ Dflat.Application/Services/JobServices/JobChangeEventArgs.cs
@@ -6,6 +6,14 @@ namespace Dflat.Application.Services.JobServices
 {
     public class JobChangeEventArgs : EventArgs
     {
+        public enum JobChangeType
+        {
+            Submitted,
+            Updated,
+            Finished
+        }
+
         public int JobID { get; set; }
+        public JobChangeType ChangeType { get; set; }
     }
 }
