4ebf57ba1ff4d175716c6f632c803ff26544deb3
Jared Lash <jared.c.lash@gmail.com>
2020-05-03 10:08:59 -0500

    Handle cancellation in FileSourceFolderScanService

--- Dflat.Application/Services/JobServices/FileSourceFolderScanService.cs
+++ Dflat.Application/Services/JobServices/FileSourceFolderScanService.cs
@@ -86,7 +86,7 @@ namespace Dflat.Application.Services.JobServices
             }
 
 
-            job.Output = ProcessFoundFiles(fileSourceFolder.Path, result);
+            job.Output = ProcessFoundFiles(fileSourceFolder.Path, result, cancellationToken);
 
             if (result.ErrorLog.Count > 0)
                 job.Status = JobStatus.SuccessWithErrors;
@@ -94,61 +94,77 @@ namespace Dflat.Application.Services.JobServices
                 job.Status = JobStatus.Success;
         }
 
-        private string ProcessFoundFiles(string path, FolderSearchServiceResult result)
+        private string ProcessFoundFiles(string path, FolderSearchServiceResult result, CancellationToken cancellationToken)
         {
             StringBuilder output = new StringBuilder();
 
-            // Set up our collections to compare
-            var beforeSearch = fileRepository.GetFromPath(path);        // "before" collection
-            List<Models.File> foundFiles = new List<Models.File>();     // "after" collection
-            
-            foreach (var fileResult in result.FoundFiles)
+            try
             {
-                Models.File newFile = mapper.Map<Models.File>(fileResult);
+                // Set up our collections to compare
+                var beforeSearch = fileRepository.GetFromPath(path);        // "before" collection
+                List<Models.File> foundFiles = new List<Models.File>();     // "after" collection
 
-                foundFiles.Add(newFile);
+                foreach (var fileResult in result.FoundFiles)
+                {
+                    Models.File newFile = mapper.Map<Models.File>(fileResult);
+
+                    foundFiles.Add(newFile);
+                }
+
+                cancellationToken.ThrowIfCancellationRequested();
+
+                // Figure out what has changed during since the last scan
+                var compareResult = comparer.Compare(beforeSearch, foundFiles);
+
+
+                foreach (var removedFile in compareResult.Removed)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    output.AppendLine($"Removed: {removedFile.Directory}{Path.DirectorySeparatorChar}{removedFile.Filename}");
+                    fileRepository.MarkRemoved(removedFile.FileID);
+                }
+
+
+                foreach (var modifiedFile in compareResult.Modified)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    output.AppendLine($"Modified: {modifiedFile.Directory}{Path.DirectorySeparatorChar}{modifiedFile.Filename}");
+                    fileRepository.Update(modifiedFile);
+                }
+
+                foreach (var addedFile in compareResult.Added)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    output.AppendLine($"Added: {addedFile.Directory}{Path.DirectorySeparatorChar}{addedFile.Filename}");
+                    fileRepository.Add(addedFile); // Sets the FileID of the added file
+                }
+
+                //Queue a MD5 and Chromaprint requests
+                foreach (var file in compareResult.Added.Concat(compareResult.Modified))
+                {
+                    var separator = new string(Path.DirectorySeparatorChar, 1);
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    var fullFilePath = string.Join(separator, file.Directory, file.Filename);
+
+
+                    //fileChromaprintService.SubmitJobRequest(new FileChromaprintJob { FileID = file.FileID, Description = "Chromaprint: " + fullFilePath });
+                    //fileMD5Service.SubmitJobRequest(new FileMD5Job { FileID = fileID, Description = "MD5: " + fullFilePath });
+                }
+
+                output.AppendLine("-----------------------------");
+                output.AppendLine($"Added: {compareResult.Added.Count} files");
+                output.AppendLine($"Removed: {compareResult.Removed.Count} files");
+                output.AppendLine($"Modified: {compareResult.Modified.Count} files");
             }
-
-            
-            // Figure out what has changed during since the last scan
-            var compareResult = comparer.Compare(beforeSearch, foundFiles);
-
-
-            foreach (var removedFile in compareResult.Removed)
+            catch (OperationCanceledException)
             {
-                output.AppendLine($"Removed: {removedFile.Directory}{Path.DirectorySeparatorChar}{removedFile.Filename}");
-                fileRepository.MarkRemoved(removedFile.FileID);
+                output.AppendLine("-----------------------------");
+                output.AppendLine("Job Cancelled");
             }
-
-
-            foreach (var modifiedFile in compareResult.Modified)
-            {
-                output.AppendLine($"Modified: {modifiedFile.Directory}{Path.DirectorySeparatorChar}{modifiedFile.Filename}");
-                fileRepository.Update(modifiedFile);
-            }
-
-            foreach (var addedFile in compareResult.Added)
-            {
-                output.AppendLine($"Added: {addedFile.Directory}{Path.DirectorySeparatorChar}{addedFile.Filename}");
-                fileRepository.Add(addedFile); // Sets the FileID of the added file
-            }
-
-            //Queue a MD5 and Chromaprint requests
-            foreach (var file in compareResult.Added.Concat(compareResult.Modified))
-            {
-                var separator = new string(Path.DirectorySeparatorChar, 1);
-               
-                var fullFilePath = string.Join(separator, file.Directory, file.Filename);
-
-                
-                //fileChromaprintService.SubmitJobRequest(new FileChromaprintJob { FileID = file.FileID, Description = "Chromaprint: " + fullFilePath });
-                //fileMD5Service.SubmitJobRequest(new FileMD5Job { FileID = fileID, Description = "MD5: " + fullFilePath });
-            }
-
-            output.AppendLine("-----------------------------");
-            output.AppendLine($"Added: {compareResult.Added.Count} files");
-            output.AppendLine($"Removed: {compareResult.Removed.Count} files");
-            output.AppendLine($"Modified: {compareResult.Modified.Count} files");
 
             return output.ToString();
         }
