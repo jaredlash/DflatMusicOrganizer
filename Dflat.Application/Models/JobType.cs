13d077eb787af18fe958fe88f0183051974bfcf3
Jared Lash <jared.c.lash@gmail.com>
2020-05-10 18:24:25 -0500

    Add MD5Job to JobType enum

--- Dflat.Application/Models/JobType.cs
+++ Dflat.Application/Models/JobType.cs
@@ -7,6 +7,7 @@ namespace Dflat.Application.Models
     public enum JobType
     {
         None = 0,
-        FileSourceFolderScanJob = 1
+        FileSourceFolderScanJob = 1,
+        MD5Job = 2
     }
 }
