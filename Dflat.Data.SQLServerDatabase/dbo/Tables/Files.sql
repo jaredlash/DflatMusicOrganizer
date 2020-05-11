2c1cb59e78a1a46ad92b3d2d1cd7b4eed392ec94
Jared Lash <jared.c.lash@gmail.com>
2020-05-11 15:26:20 -0500

    Limit lengths of Filenames and Directories, add index to Directory

--- Dflat.Data.SQLServerDatabase/dbo/Tables/Files.sql
+++ Dflat.Data.SQLServerDatabase/dbo/Tables/Files.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Files]
 (
 	[FileID] INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-	[Filename] NVARCHAR(MAX) NOT NULL,
+	[Filename] NVARCHAR(300) NOT NULL,
 	[Extension] NVARCHAR(30) NOT NULL,
-	[Directory] NVARCHAR(MAX) NOT NULL, 
+	[Directory] NVARCHAR(300) NOT NULL, 
     [Size] INT NOT NULL,
 	[LastModifiedTime] DATETIME2 NOT NULL,
 	[MarkedAsRemoved] BIT NOT NULL DEFAULT(0),
@@ -13,3 +13,7 @@
 GO
 
 CREATE INDEX [IX_Files_MD5Sum] ON [dbo].[Files] ([MD5Sum])
+
+GO
+
+CREATE INDEX [IX_Files_Directory] ON [dbo].[Files] ([Directory])
