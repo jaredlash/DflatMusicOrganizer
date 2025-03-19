c4845e755a7ae09278d82c45d9b1aeefd6e46277
Jared Lash <jared.c.lash@gmail.com>
2025-03-19 13:46:35 -0500

    Use file-scoped namespace

--- DflatMusicOrganizer/DflatCoreWPF/ViewModels/ViewModelExtensions.cs
+++ DflatMusicOrganizer/DflatCoreWPF/ViewModels/ViewModelExtensions.cs
@@ -1,23 +1,22 @@
 ﻿using Dflat.Application.Models;
 
-namespace DflatCoreWPF.ViewModels
+namespace DflatCoreWPF.ViewModels;
+
+public static class ViewModelExtensions
 {
-    public static class ViewModelExtensions
+    public static void SetFromViewModel(this FileSourceFolder fileSourceFolder, FileSourceFolderEditorViewModel viewModel)
     {
-        public static void SetFromViewModel(this FileSourceFolder fileSourceFolder, FileSourceFolderEditorViewModel viewModel)
+        fileSourceFolder.FileSourceFolderID = viewModel.FileSourceFolderID;
+        fileSourceFolder.Name = viewModel.Name;
+        fileSourceFolder.Path = viewModel.Path;
+        fileSourceFolder.IsTemporaryMedia = viewModel.IsTemporaryMedia;
+        fileSourceFolder.LastScanStart = viewModel.LastScanStart;
+        fileSourceFolder.IsChanged = viewModel.IsChanged;
+        // Copy exclude paths
+        fileSourceFolder.ExcludePaths.Clear();
+        foreach (var excludePath in viewModel.ExcludePaths)
         {
-            fileSourceFolder.FileSourceFolderID = viewModel.FileSourceFolderID;
-            fileSourceFolder.Name = viewModel.Name;
-            fileSourceFolder.Path = viewModel.Path;
-            fileSourceFolder.IsTemporaryMedia = viewModel.IsTemporaryMedia;
-            fileSourceFolder.LastScanStart = viewModel.LastScanStart;
-            fileSourceFolder.IsChanged = viewModel.IsChanged;
-            // Copy exclude paths
-            fileSourceFolder.ExcludePaths.Clear();
-            foreach (var excludePath in viewModel.ExcludePaths)
-            {
-                fileSourceFolder.ExcludePaths.Add(excludePath);
-            }
+            fileSourceFolder.ExcludePaths.Add(excludePath);
         }
     }
 }
