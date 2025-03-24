211593f397f09d6c25374b715f650e7bdb8ac0e3
Jared Lash <jared.c.lash@gmail.com>
2025-03-23 21:59:11 -0500

    Use file-scoped namespaces, remove unneeded usings, enable nullable reference types on Repo interfaces

--- DflatMusicOrganizer/Dflat.Application/Repositories/IJobRepository.cs
+++ DflatMusicOrganizer/Dflat.Application/Repositories/IJobRepository.cs
@@ -1,42 +1,39 @@
 ﻿using Dflat.Application.Models;
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Dflat.Application.Repositories
+namespace Dflat.Application.Repositories;
+
+public interface IJobRepository
 {
-    public interface IJobRepository //: IRepository<Job>
-    {
-        void Add<JobType>(JobType job) where JobType : Job;
+    void Add<JobType>(JobType job) where JobType : Job;
 
-        void Update<JobType>(JobType job) where JobType : Job;
+    void Update<JobType>(JobType job) where JobType : Job;
 
-        bool RestartJob(int jobID);
+    bool RestartJob(int jobID);
 
-        bool CancelJob(int jobID);
+    bool CancelJob(int jobID);
 
-        /// <summary>
-        /// Retrieves the next available job of the specified type.  Job is retrieved in the "Running" state.
-        /// </summary>
-        /// <typeparam name="JobType"></typeparam>
-        /// <returns></returns>
-        JobType GetNextAvailable<JobType>() where JobType : Job;
+    /// <summary>
+    /// Retrieves the next available job of the specified type.  Job is retrieved in the "Running" state.
+    /// </summary>
+    /// <typeparam name="JobType"></typeparam>
+    /// <returns></returns>
+    JobType GetNextAvailable<JobType>() where JobType : Job;
 
-        //bool PrerequisitesFinished(int jobID);
-        Job Get(int jobID);
+    Job Get(int jobID);
 
-        IEnumerable<JobInfo> GetJobInfoByCriteria(JobType jobType = JobType.None, JobStatus status = JobStatus.None);
+    IEnumerable<JobInfo> GetJobInfoByCriteria(JobType jobType = JobType.None, JobStatus status = JobStatus.None);
 
-        Task<IEnumerable<JobInfo>> GetJobInfoByCriteriaAsync(JobType jobType = JobType.None, JobStatus status = JobStatus.None);
+    Task<IEnumerable<JobInfo>> GetJobInfoByCriteriaAsync(JobType jobType = JobType.None, JobStatus status = JobStatus.None);
 
-        JobInfo GetJobInfo(int jobID);
+    JobInfo GetJobInfo(int jobID);
 
-        int GetJobCount();
+    int GetJobCount();
 
-        int GetJobCountByStatus(JobStatus jobStatus);
+    int GetJobCountByStatus(JobStatus jobStatus);
 
-        int GetQueuedJobCount();
+    int GetQueuedJobCount();
 
-        int GetRunningJobCount();
-    }
+    int GetRunningJobCount();
 }
